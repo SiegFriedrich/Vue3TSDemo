9f77ff8d8218608787c8686f30f19dae7847207e
Sieg Friedrich <siegfriedrich@outlook.com>
2023-02-13 20:33:26 +0900

    add UserView GoodsView

--- src/router/index.ts
+++ src/router/index.ts
@@ -8,6 +8,34 @@ const routes: Array<RouteRecordRaw> = [
   //   component: HomeView,
   // },
   {
+    path: "/",
+    name: "main",
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/MainView.vue"),
+    children: [
+      {
+        path: "/goods",
+        name: "goods",
+        meta: {
+          isShow: true,
+          title: "商品列表",
+        },
+        component: () =>
+          import(/* webpackChunkName: "about" */ "../views/GoodsView.vue"),
+      },
+      {
+        path: "/users",
+        name: "users",
+        meta: {
+          isShow: true,
+          title: "用户列表",
+        },
+        component: () =>
+          import(/* webpackChunkName: "about" */ "../views/UsersView.vue"),
+      },
+    ],
+  },
+  {
     path: "/about",
     name: "about",
     // route level code-splitting
@@ -16,15 +44,15 @@ const routes: Array<RouteRecordRaw> = [
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
   },
-  {
-    path: "/",
-    name: "login",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "login" */ "../views/LoginView.vue"),
-  },
+  // {
+  // path: "/",
+  // name: "login",
+  // route level code-splitting
+  // this generates a separate chunk (about.[hash].js) for this route
+  // which is lazy-loaded when the route is visited.
+  // component: () =>
+  //   import(/* webpackChunkName: "login" */ "../views/LoginView.vue"),
+  // },
   {
     path: "/blog-main-page",
     name: "blogMainPage",
---
+++ src/views/MainView.vue
@@ -0,0 +1,95 @@
+<template>
+    <div class="common-layout">
+      <el-container>
+        <el-header class="header">
+        <div><img alt="vue.logo" src="../assets/logo.png"></div>
+           <h2>后台管理系统</h2>
+         <div class="logout">Logout</div>
+        </el-header>
+        <el-container>
+        <el-aside class="aside" width="200px">
+       
+           
+
+      <el-menu
+        active-text-color="#ffd04b"
+        background-color="#545c64"
+        class="el-menu-vertical-demo"
+        default-active="2"
+        text-color="rgb(247,153,74)"
+        router
+      >
+        <el-menu-item v-for="item in routerList" :index="item.path" :key="item.path">
+         
+          <span>{{item.meta.title}}</span>
+        </el-menu-item>
+
+      </el-menu>
+
+
+        
+   
+
+        </el-aside>
+        <el-main><router-view></router-view></el-main>
+      </el-container>
+      </el-container>
+    </div>
+</template>
+  
+  
+
+<script lang="ts">
+import { defineComponent} from 'vue'
+import router from '@/router';
+
+
+export default defineComponent({
+    setup () {
+      const handleOpen = (key: string, keyPath: string[]) => {
+  console.log(key, keyPath)
+}
+const handleClose = (key: string, keyPath: string[]) => {
+  console.log(key, keyPath)
+}
+      const routerList = router.getRoutes().filter( el => el.meta.isShow == true);
+      console.log(routerList)
+        
+        
+        
+
+        return {routerList,handleOpen,handleClose}
+    }
+})
+</script>
+
+<style lang="scss" scoped>
+  .header{
+    height: 80px;
+    background-color: burlywood;
+    color: white;
+    display: flex;
+    justify-content:space-between;
+    img{
+      width: 80px;
+    }
+    h2,.logout{
+      text-align: center;
+      height: 80px;
+      line-height: 80px;
+      color:aliceblue
+    }
+    .grid4{
+      background-color: aquamarine;
+    }
+  }
+  .aside{
+    .el-menu-vertical-demo{
+      background-color:#545c64;
+      color: white;
+      /** 100vh means the height of screen */
+      height: calc( 100vh - 80px )
+    }
+
+  }
+</style>
