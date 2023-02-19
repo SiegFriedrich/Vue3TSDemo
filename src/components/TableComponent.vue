1efcfe07e6008f368c4aaa6c260ee2e49c5aa47b
Sieg Friedrich <siegfriedrich@outlook.com>
2023-02-19 15:17:22 +0900

    add pagination feature

--- src/components/TableComponent.vue
+++ src/components/TableComponent.vue
@@ -1,17 +1,24 @@
 <template>
-    <el-table :data="data.list" stripe style="width: 100%">
+    <el-table :data="dataList" stripe style="width: 100%">
+        <el-table-column prop="id" label="ID" width="100" />
+
         <el-table-column prop="title" label="Title" width="180" />
         <el-table-column prop="info" label="Introduction" />
         <el-table-column prop="company" label="Company" width="180" />
     </el-table>
     <div class="example-pagination-block">
-        <el-pagination layout="prev, pager, next" :total="data.list.length" />
+        <div class="demo-pagination-block">
+            <el-pagination v-model:current-page="data.selectData.page" v-model:page-size="data.selectData.pageSize"
+                layout="prev, pager, next, jumper" :total="data.list.length" @size-change="handleSizeChange"
+                @current-change="handleCurrentChange" />
+        </div>
 </div>
 </template>
   
 <script lang="ts" setup>
-import { PropType, defineProps, toRefs } from 'vue';
+import { PropType, defineProps, toRefs, reactive } from 'vue';
 import { initData } from '../models/goods.model';
+import { computed } from '@vue/reactivity';
 const props = defineProps({
     data: {
         required: true,
@@ -20,7 +27,17 @@ const props = defineProps({
 });
 //You can use this kinda method/way to retrieve property from props!
 const { data } = toRefs(props);
-data.value.list.length
+const dataList = reactive(computed(() => {
+    return data.value.list.slice((data.value.selectData.page - 1) * data.value.selectData.pageSize,
+        (data.value.selectData.page * data.value.selectData.pageSize));
+}));
+const handleSizeChange = (pageSize: number) => {
+    data.value.selectData.pageSize = pageSize;
+}
+
+const handleCurrentChange = (currentPage: number) => {
+    data.value.selectData.page = currentPage;
+}
 
 </script>
   
